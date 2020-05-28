54f8a540916bc1f3464a24eba323eccc2051aa52
wright <calebogundiya@gmail.com>
2020-05-28 22:59:57 +0100

    Index configurations

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/*
+*  	Plugin Name: WC Ondeot Distance Checkout
+*  	Author: Wright
+*	Description: .
+*	Version: 0.0.1
+*	Author URI: https://www.github.com/wrightkhlebisol/
+*	Description: 
+*/
+
+// Includes
+
+// Shortcodes
+
+// Hooks
+
